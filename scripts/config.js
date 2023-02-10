b686dcaa458c89c9edb43c912813d39ac171d5bc
Wichapon V <wichapon@gmail.com>
2023-02-10 14:30:59 +0700

    add eventlistener to edit button to display overlay ui

---
+++ scripts/config.js
@@ -0,0 +1,10 @@
+
+function playerConfig(){
+    playerOverlay.style.display='block';
+    backdropElement.style.display='block';
+}
+
+function cancelBtnConfig(){
+    playerOverlay.style.display = 'none';
+    backdropElement.style.display= 'none'
+}
