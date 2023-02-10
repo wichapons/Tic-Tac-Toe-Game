352e4d124ed8c3356d86dde5fdd0a11001d1ddb6
Wichapon V <wichapon@gmail.com>
2023-02-11 01:10:00 +0700

    add validate player name input

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
 
-//edit button
+//edit button on main page
 const editPlayer1Btn = document.querySelector('#edit-player1-btn');
 const editPlayer2Btn = document.querySelector('#edit-player2-btn');
 console.log(editPlayer1Btn);
@@ -8,9 +8,16 @@ console.log(editPlayer1Btn);
 const playerOverlay = document.querySelector('#overlay-config');
 const backdropElement = document.querySelector('#backdrop');
 const cancelBtn = document.querySelector('#overlay-config .btn-alt');
-console.log(cancelBtn);
+const inputFormElement = document.querySelector('.overlay form');
+//inputwarning
+const inputWarning =  document.querySelector('#enter-name-warning');
 
-
+//event listener//
+//edit button 
 editPlayer1Btn.addEventListener('click',playerConfig);
 editPlayer2Btn.addEventListener('click',playerConfig);
-cancelBtn.addEventListener('click',cancelBtnConfig)
+//cancel button 
+cancelBtn.addEventListener('click',closeOverlay);
+//input player name 
+inputFormElement.addEventListener('submit',savePlayerName)
+
--- scripts/config.js
+++ scripts/config.js
@@ -1,10 +1,28 @@
 
-function playerConfig(){
+function playerConfig(event){
+    const playerNum = event.target.dataset.playerNum; //if dataset has dash(-) like player-Num => dataset['playerNum'] because cannot access to object with dash with dot command
     playerOverlay.style.display='block';
     backdropElement.style.display='block';
 }
 
-function cancelBtnConfig(){
+function closeOverlay(){
     playerOverlay.style.display = 'none';
-    backdropElement.style.display= 'none'
+    backdropElement.style.display= 'none';
+    inputWarning.textContent='';
+}
+
+function savePlayerName(event){
+    event.preventDefault();
+    /*
+    const formData = new FormData(event.target);
+    const inputPlayerName = formData.get('username');
+    */
+    const inputPlayerName = document.querySelector("#playername").value.trim(); // '      top lop    '  => 'top lop'
+    
+    if (!inputPlayerName){
+       inputWarning.textContent = 'please enter the name';
+       inputWarning.style.display = 'block'
+    }
+
+
 }
